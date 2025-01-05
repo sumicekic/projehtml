e9d8f9362d1066f4833f257c388050c432891635
sumicekic <sumeyye.cekic@stu.pirireis.edu.tr>
2025-01-05 18:52:41 +0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Leaflet harita ayarları
-var map = L.map('harita').setView([40.9484, 29.2971], 13); // Tuzla Piri Reis Üniversitesi'nin koordinatları
+var map = L.map('harita').setView([40.815273, 29.291231], 15); // Piri Reis Üniversitesi'nin koordinatları
 
 // Harita TileLayer ekle
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,9 +7,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Harita üzerine konum işareti ekle
-L.marker([40.9484, 29.2971]).addTo(map)
-  .bindPopup("Tuzla Piri Reis Üniversitesi")
+L.marker([40.815273, 29.291231]).addTo(map)
+  .bindPopup("Piri Reis Üniversitesi<br>Postane Mahallesi, Eflatun Sokak, No:8, 34940 Tuzla/İstanbul")
   .openPopup();
+
 
 // Form kontrol ve geri bildirim
 document.getElementById('contact-form').addEventListener('submit', function(event) {
@@ -24,8 +25,56 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   // Hata kontrolü
   if (!adSoyad || !eposta || !konu || !mesaj) {
     document.getElementById('feedbackMessage').style.display = 'block';
+    document.getElementById('successMessage').style.display = 'none';
   } else {
     document.getElementById('feedbackMessage').style.display = 'none';
+
+    // Dinamik mesaj oluştur
+    var dynamicMessage = `Sayın ${adSoyad}, "${konu}" konulu "${mesaj}" mesajınız tarafımıza ulaşmıştır. Sizinle en kısa sürede iletişime geçeceğiz.`;
+
+    // Mesajı göster
+    document.getElementById('successMessage').innerText = dynamicMessage;
     document.getElementById('successMessage').style.display = 'block';
   }
 });
+
+// Ad Soyad kontrol fonksiyonu
+function kontrolAdSoyad() {
+    const adSoyad = document.getElementById('AdSoyad').value;
+    if (!adSoyad) {
+      alert('Lütfen isim ve soy isminizi giriniz.');
+    }
+  }
+  
+  // Telefon kontrol fonksiyonu
+  function kontrolTelefon() {
+    const telefon = document.getElementById('Telefon').value;
+    if (!telefon) {
+      alert('Lütfen telefon numaranızı giriniz.');
+    }
+  }
+  
+  // E-posta kontrol fonksiyonu
+  function kontrolEposta() {
+    const eposta = document.getElementById('Eposta').value;
+    if (!eposta) {
+      alert('Lütfen e-posta adresinizi giriniz.');
+    }
+  }
+  
+  // Konu kontrol fonksiyonu
+  function kontrolKonu() {
+    const konu = document.getElementById('Konu').value;
+    if (!konu) {
+      alert('Lütfen mesaj konusunu giriniz.');
+    }
+  }
+  
+  // Mesaj kontrol fonksiyonu
+  function kontrolMesaj() {
+    const mesaj = document.getElementById('Mesaj').value;
+    if (!mesaj) {
+      alert('Lütfen mesajınızı yazınız.');
+    }
+  }
+  
